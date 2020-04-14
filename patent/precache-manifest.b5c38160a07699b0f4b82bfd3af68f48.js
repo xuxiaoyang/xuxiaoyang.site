f1b8bf404926d6e3bf76eb7afec2280d778aaced
xuxiaoyang <xuxiaoyang519@gmail.com>
2020-04-14 12:00:42 +0800

    Site updated: 2020-04-14 12:00:40

---
+++ patent/precache-manifest.b5c38160a07699b0f4b82bfd3af68f48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20db7b0d54fe4736a60dfe7e6aa3976e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0e59a0d3f4c7da0104a",
+    "url": "./static/css/2.b21a8814.chunk.css"
+  },
+  {
+    "revision": "43b0bceb12eb61ec7290",
+    "url": "./static/css/main.58ac90bd.chunk.css"
+  },
+  {
+    "revision": "b0e59a0d3f4c7da0104a",
+    "url": "./static/js/2.440e541f.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "./static/js/2.440e541f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43b0bceb12eb61ec7290",
+    "url": "./static/js/main.eb0fb640.chunk.js"
+  },
+  {
+    "revision": "61d1ca7ac0debc180f47",
+    "url": "./static/js/runtime-main.6fd61394.js"
+  },
+  {
+    "revision": "1a2e128cc337b9577da634ed6f874aed",
+    "url": "./static/media/map_bg.1a2e128c.png"
+  }
+]);
