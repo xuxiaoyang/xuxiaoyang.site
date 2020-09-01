536a002872b2827ae6d77fe10944ef1961b4b43e
xxvis <1989519xxy@163.com>
2020-09-01 15:44:19 +0800

    Site updated: 2020-09-01 15:44:17

--- cstpcd/service-worker.js
+++ cstpcd/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.138c5ee4e4d9c4483f55b93d071a9153.js"
+  "./precache-manifest.669f5425b2b57bd505a97f248e0f4c52.js"
 );
 
 self.addEventListener('message', (event) => {
